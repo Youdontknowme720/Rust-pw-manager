f95aea057c408c121a792a01a111b2c19ae15a8c
ayanokouji <wenzelmannnico1@gmail.com>
2026-07-18 18:02:50 +0200

    added find function in password_manager.rs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
 use crate::manager::password_manager::PasswordManager;
-use crate::models::password_entry::PasswordEntry;
 use crate::storage::json_storage::JsonStorage;
 
+mod manager;
 mod models;
 mod storage;
-mod manager;
 
 fn main() {
     let s = JsonStorage::new("password.json");
     let manager: PasswordManager = PasswordManager::new(s);
-    let new_entry: PasswordEntry = PasswordEntry{
-        service: "Uber".to_string(),
-        url: "asjkdf".to_string(),
-        username: "Ayanokouji".to_string(),
-        password: "Whoknows".to_string(),
-        additional_nodes: Some("Ayanokouji".to_string()),
-    };
-    match manager.add(new_entry){
-        Ok(_) => println!("Saved Password"),
-        Err(e) => println!("{}", e)
+    match manager.find("Uber".to_string()) {
+        Ok(entry) => println!("Found -> {:?}", entry),
+        Err(_) => println!("Couldn't find entry"),
     };
 }
--- src/manager/password_manager.rs
+++ src/manager/password_manager.rs
@@ -27,4 +27,19 @@ impl PasswordManager {
 
         Ok(())
     }
+
+    pub fn find(&self, password_id: String)-> Result<PasswordEntry, String>{
+        let load_content = self.storage.load();
+        let entries = match load_content {
+            Ok(e) => e,
+            Err(e) => return Err(e.to_string()),
+        };
+
+        for ent in entries{
+            if *ent.service == password_id{
+                return Ok(ent)
+            }
+        }
+        Err("Password not found".to_string())
+    }
 }
